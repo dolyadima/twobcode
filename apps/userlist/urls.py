e7e6c7f27f5c1026105f74efc08e3e1bc491f839
dolyadima <dimchik96@gmail.com>
2022-07-17 21:38:08 +0300

    file permissions changed, add delete user

--- apps/userlist/urls.py
+++ apps/userlist/urls.py
@@ -5,4 +5,5 @@ app_name = 'userlist'
 urlpatterns = [
     path('', views.users, name='users'),
     path('user_detail/<int:pk>/', views.user_detail, name='user_detail'),
+    path('user_delete/<int:pk>/', views.user_delete, name='user_delete'),
 ]
--- apps/userlist/views.py
+++ apps/userlist/views.py
@@ -7,18 +7,33 @@ def users(request):
         'title': 'User List',
         'page_active': 'users',
         'users': User.objects.all().order_by("id"),
-        'user': request.user,
+        'req_user': request.user,
     })
 
 
 def user_detail(request, pk):
-    user_d = User.objects.filter(id=pk).first()
-    id_not_found = True if user_d is None else False
+    user_detail = User.objects.filter(id=pk).first()
+    id_not_found = True if user_detail is None else False
     return render(request, 'userlist/user_detail.html', {
         'title': 'User Detail',
         'page_active': 'users',
         'id_not_found': id_not_found,
         'pk': pk,
-        'user_d': user_d,
-        'user': request.user,
+        'user_detail': user_detail,
+        'req_user': request.user,
+    })
+
+
+def user_delete(request, pk):
+    user_delete = User.objects.filter(id=pk).first()
+    id_not_found = True if user_delete is None else False
+    if not id_not_found:
+        user_delete.delete()
+    return render(request, 'userlist/user_delete.html', {
+        'title': 'User Delete',
+        'page_active': 'users',
+        'id_not_found': id_not_found,
+        'pk': pk,
+        'user_delete': user_delete,
+        'req_user': request.user,
     })
